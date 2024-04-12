7aaafc9abbb24e0677203e0a9083a85fb29cd93e
Ysiva@8689 <ysivaparvathi23@gmail.com>
2024-04-11 22:34:45 -0400

    first commit

---
+++ Assignment-1.R
@@ -0,0 +1,33 @@
+# Create a vector with the given numbers
+numbers <- c(7, 12, 5)
+
+# Calculate the average
+average <- mean(numbers)
+
+# Print the result
+cat("The average of the numbers is:", average)
+
+# Replace [your first name] with your actual first name
+first_name <- "Sivaparvathi"  # Change this to your name
+
+# Print the customized message
+cat("Welcome to R, ", first_name, "\n")
+
+# Define the variables
+x <- 7
+y <- 12
+z <- 4
+
+# Compare x with y
+if (x > y) {
+  cat("x is bigger than y.\n")
+} else {
+  cat("x is not bigger than y.\n")
+}
+
+# Compare x with z
+if (x > z) {
+  cat("x is bigger than z.\n")
+} else {
+  cat("x is not bigger than z.\n")
+}
